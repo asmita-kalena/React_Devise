3b1b2e71399b7b4ba218aa32090671c6edccd14f
C240 <c240@C240s-iMac.local>
2017-06-26 12:54:58 +0530

    set heroku url in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,7 +86,7 @@ Rails.application.configure do
 
   #send mailer
 
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.default_url_options = { :host => 'https://react-devise.herokuapp.com' }
 
   config.action_mailer.raise_delivery_errors = true
 
